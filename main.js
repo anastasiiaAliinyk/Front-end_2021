bdcd0e917f629f9d39eeab19a2fcc7bc916fca21
Anastasiia Aliinyk <anastasiia.aliinyk@gmail.com>
2021-05-26 16:43:22 +0300

    Updates

--- main.js
+++ main.js
@@ -4,4 +4,12 @@ const burgerMenu = document.querySelector('.l-burger-menu_nav');
 btnMenu.addEventListener('click', () => {
   btnMenu.classList.toggle('active');
   burgerMenu.classList.toggle('active');
-})
+});
+
+const buttonsSelectPlus = document.querySelectorAll('.item__select');
+
+buttonsSelectPlus.forEach(buttonSelectPlus => {
+  buttonSelectPlus.addEventListener('click', () => {
+    buttonSelectPlus.classList.toggle('active');
+  });
+});
