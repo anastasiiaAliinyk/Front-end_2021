707ba1506606d5f905d38ccabf3fc10f417b7596
Anastasiia Aliinyk <anastasiia.aliinyk@gmail.com>
2021-05-30 23:27:39 +0300

    header with new structure and classes

--- src/main.js
+++ src/main.js
@@ -1,26 +1,58 @@
-const btnMenu = document.querySelector('.btn-menu');
-const burgerMenu = document.querySelector('.l-burger-menu_nav');
+//Change top menu styles when scrolling
+const topMenu = document.querySelector('#menu');
+const topMenuContainer = document.querySelector('.header-top');
+const tabsMenu = document.querySelector('.tabs-menu');
+
+window.addEventListener('scroll', function(e) {
+  const headerHeight = document.querySelector('.header').clientHeight;
+  const isActiveTopMenu = document.querySelectorAll('.header__top-nav.active').length;
+
+  const top = window.scrollY;
+
+  if (top >= headerHeight && !isActiveTopMenu) {
+    topMenu.classList.add('scroll-styles');
+    topMenuContainer.style.background = 'white';
+
+  } else {
+    topMenu.classList.remove('scroll-styles');
+    topMenuContainer.style.background = 'none';
+  }
+
+  if(top >= (headerHeight - topMenu.clientHeight + 10)) {
+    tabsMenu.classList.add('active');
+  } else {
+    tabsMenu.classList.remove('active');
+  }
+});
+
+const btnMenu = document.querySelector('.header__burger');
+const menu = document.querySelector('.menu');
 
 btnMenu.addEventListener('click', () => {
   btnMenu.classList.toggle('active');
-  burgerMenu.classList.toggle('active');
+  topMenu.classList.toggle('active');
+  menu.classList.toggle('active');
 });
 
-const buttonsSelectPlus = document.querySelectorAll('.item__select');
 
-buttonsSelectPlus.forEach(buttonSelectPlus => {
-  buttonSelectPlus.addEventListener('click', (e) => {
-    buttonSelectPlus.classList.toggle('active');
 
-    let hiddenContent = e.target.previousElementSibling;
-    let itemNumber = hiddenContent.previousElementSibling;
 
-      if (hiddenContent.style.display === "block") {
-        hiddenContent.style.display = "none";
-        itemNumber.style.display = "flex";
-      } else {
-        hiddenContent.style.display = "block";
-        itemNumber.style.display = "none";
-      }
-  });
-});
+
+// const buttonsSelectPlus = document.querySelectorAll('.item__select');
+
+// buttonsSelectPlus.forEach(buttonSelectPlus => {
+//   buttonSelectPlus.addEventListener('click', (e) => {
+//     buttonSelectPlus.classList.toggle('active');
+
+//     let hiddenContent = e.target.previousElementSibling;
+//     let itemNumber = hiddenContent.previousElementSibling;
+
+//       if (hiddenContent.style.display === "block") {
+//         hiddenContent.style.display = "none";
+//         itemNumber.style.display = "flex";
+//       } else {
+//         hiddenContent.style.display = "block";
+//         itemNumber.style.display = "none";
+//       }
+//   });
+// });
