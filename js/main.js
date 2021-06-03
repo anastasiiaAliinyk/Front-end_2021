42931b460ecb09be8846487fc201f3ce5a21e33f
Anastasiia Aliinyk <anastasiia.aliinyk@gmail.com>
2021-06-03 15:37:27 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -3,19 +3,37 @@ const topMenu = document.querySelector('#menu');
 const topMenuContainer = document.querySelector('.header-top');
 const tabsMenu = document.querySelector('.tabs-menu');
 
+//ACTIVE SLIDER
+let prevActive = document.querySelector('.slider-link');
+prevActive.classList.add('active');
+
+document.querySelector('.slider').addEventListener('click', (e) => {
+  if (e.target.classList.contains('slider-link')) {
+    e.preventDefault();
+
+    prevActive.classList.remove('active');
+    e.target.classList.add('active');
+    prevActive = e.target;
+  }
+});
+
 window.addEventListener('scroll', function(e) {
   const headerHeight = document.querySelector('.header').clientHeight;
-  const isActiveTopMenu = document.querySelectorAll('.header__top-nav.active').length;
 
   const top = window.scrollY;
+  const width = window.screen.width; 
 
-  if (top >= headerHeight && !isActiveTopMenu) {
+  if (top >= headerHeight && width > 1120) {
     topMenu.classList.add('scroll-styles');
     topMenuContainer.style.background = 'white';
 
   } else {
     topMenu.classList.remove('scroll-styles');
     topMenuContainer.style.background = 'none';
+  }
+
+  if (top >= headerHeight && width < 1120) {
+    topMenuContainer.style.background = 'linear-gradient(to left, #00f473 15%, #22c6d4 80%, #17c3e1)';
   }
 
   if(top >= (headerHeight - topMenu.clientHeight + 10)) {
@@ -34,8 +52,7 @@ btnMenu.addEventListener('click', () => {
   menu.classList.toggle('active');
 });
 
-//SLIDER
-
+//SLIDER PROPERTIES
 $(document).ready(function() {
   $('.slider').slick({
     slidesToScroll: 1,
@@ -63,10 +80,8 @@ $(document).ready(function() {
   });
 
 
+//LOGIC FOR SLIDER
   $('.slider-item').click(function(e) {
-
-    // e.target.classList.add('active');
-
     clickedIndex = $(this).data("slick-index");
 
     $('.slider-big').slick('slickGoTo', clickedIndex, true);
