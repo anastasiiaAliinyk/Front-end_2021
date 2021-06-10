fa240b8db60640e7117245f512a0987d28f3c667
Anastasiia Aliinyk <anastasiia.aliinyk@gmail.com>
2021-06-10 20:06:48 +0300

    scout, companies, footer blocks

--- 2/src/js/main.js
+++ 2/src/js/main.js
@@ -1,8 +1,66 @@
 const burgerBtn = document.querySelector(".header-burger");
 const menu = document.querySelector(".menu");
+const header = document.querySelector(".header");
 
 burgerBtn.addEventListener("click", () => {
-  console.log(burgerBtn)
   burgerBtn.classList.toggle("active");
   menu.classList.toggle("active");
 });
+
+//Change menu color when scrolling
+window.addEventListener("scroll", function(e) {
+  const headerHeight = document.querySelector('.heading').clientHeight;
+
+  const top = window.scrollY;
+
+  if (top >= headerHeight) {
+    header.classList.add("scroll-styles");
+    menu.classList.add("scroll-styles");
+  } else {
+    header.classList.remove("scroll-styles");
+    menu.classList.remove("scroll-styles");
+  }
+});
+
+jQuery(function() {
+  $(".ventures-slider").slick({
+    speed: 300,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: false,
+    infinite: true,
+    fade: true,
+    asNavFor: ".ventures-slider-small"
+  });
+
+  $(".ventures-slider-small").slick({
+    infinite: false,
+    arrows: false,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    focusOnSelect: true,
+    asNavFor: '.ventures-slider',
+  });
+
+
+  if (window.matchMedia("(max-width: 1150px)").matches) {
+    $(".plans-slider").slick({
+      infinite: true,
+      arrows: false,
+      speed: 300,
+      slidesToShow: 1,
+      autoplay: true,
+      autoplaySpeed: 1500,
+      variableWidth: true,
+    });
+  }
+});
+
+
+//LOGIC FOR SLIDER
+//   $('.slider-item').click(function(e) {
+//     clickedIndex = $(this).data("slick-index");
+
+//     $('.slider-big').slick('slickGoTo', clickedIndex, true);
+//   });
+// });
