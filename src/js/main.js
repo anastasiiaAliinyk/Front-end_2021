3d9cc9263f1d895dc66f4019eda88782d04d805c
Anastasiia Aliinyk <anastasiia.aliinyk@gmail.com>
2021-06-13 19:16:25 +0300

    Updates

--- src/js/main.js
+++ src/js/main.js
@@ -1,48 +1,65 @@
-const burgerBtn = document.querySelector(".header-burger");
-const menu = document.querySelector(".menu");
-const header = document.querySelector(".header");
+const PLANS_SLIDER_SELECTOR = '.plans-slider';
+const PROBLEMS_SLIDER_SELECTOR = '.problems-slider';
+const VENTURES_SLIDER_SELECTOR = '.ventures-slider';
+const VENTURES_SMALL_SLIDER_SELECTOR = '.ventures-slider-small';
 
-burgerBtn.addEventListener("click", () => {
-  burgerBtn.classList.toggle("active");
-  menu.classList.toggle("active");
+const burgerBtn = document.querySelector('.header-burger');
+const menu = document.querySelector('.menu');
+const header = document.querySelector('.header');
+
+burgerBtn.addEventListener('click', () => {
+  burgerBtn.classList.toggle('active');
+  menu.classList.toggle('active');
+});
+
+window.addEventListener('resize', function () {
+  if (document.querySelector(PLANS_SLIDER_SELECTOR).classList.contains('slick-initialized')) {
+    if (!window.matchMedia('(max-width: 1240px)').matches) {
+      unSlickPlansSlider();
+    }
+  } else {
+    if (window.matchMedia('(max-width: 1240px)').matches) {
+      slickPlansSlider();
+    }
+  }
 });
 
 //Change menu color when scrolling
-window.addEventListener("scroll", function(e) {
+window.addEventListener('scroll', function (e) {
   const headerHeight = document.querySelector('.heading').clientHeight;
 
   const top = window.scrollY;
 
   if (top >= headerHeight) {
-    header.classList.add("scroll-styles");
-    menu.classList.add("scroll-styles");
+    header.classList.add('scroll-styles');
+    menu.classList.add('scroll-styles');
   } else {
-    header.classList.remove("scroll-styles");
-    menu.classList.remove("scroll-styles");
+    header.classList.remove('scroll-styles');
+    menu.classList.remove('scroll-styles');
   }
 });
 
-jQuery(function() {
-  $(".ventures-slider").slick({
+jQuery(function () {
+  $(VENTURES_SLIDER_SELECTOR).slick({
     speed: 300,
-    // autoplay: true,
-    // autoplaySpeed: 1500,
+    autoplay: true,
+    autoplaySpeed: 1500,
     arrows: false,
     infinite: true,
     fade: true,
-    asNavFor: ".ventures-slider-small"
+    asNavFor: VENTURES_SMALL_SLIDER_SELECTOR
   });
 
-  $(".ventures-slider-small").slick({
+  $(VENTURES_SMALL_SLIDER_SELECTOR).slick({
     infinite: false,
     arrows: false,
     slidesToShow: 5,
     slidesToScroll: 5,
     focusOnSelect: true,
-    asNavFor: '.ventures-slider',
+    asNavFor: VENTURES_SLIDER_SELECTOR
   });
 
-  $(".problems-slider").slick({
+  $(PROBLEMS_SLIDER_SELECTOR).slick({
     arrows: false,
     dots: true,
     speed: 300,
@@ -50,27 +67,26 @@ jQuery(function() {
     fade: true,
     cssEase: 'linear',
     autoplay: true,
-    autoplaySpeed: 1500,
+    autoplaySpeed: 1500
   });
 
-  if (window.matchMedia("(max-width: 1150px)").matches) {
-    $(".plans-slider").slick({
-      infinite: true,
-      arrows: false,
-      speed: 300,
-      slidesToShow: 1,
-      // autoplay: true,
-      // autoplaySpeed: 1500,
-      variableWidth: true,
-    });
+  if (window.matchMedia('(max-width: 1240px)').matches) {
+    slickPlansSlider();
   }
 });
 
+function slickPlansSlider () {
+  $(PLANS_SLIDER_SELECTOR).slick({
+    infinite: true,
+    arrows: false,
+    speed: 300,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    variableWidth: true,
+  });
+}
 
-//LOGIC FOR SLIDER
-//   $('.slider-item').click(function(e) {
-//     clickedIndex = $(this).data("slick-index");
-
-//     $('.slider-big').slick('slickGoTo', clickedIndex, true);
-//   });
-// });
+function unSlickPlansSlider () {
+  $(PLANS_SLIDER_SELECTOR).slick('unslick');
+}
