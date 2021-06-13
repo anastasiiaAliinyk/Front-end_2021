2f0b16e58a059855a13c1197ba305bf40ab65932
Anastasiia Aliinyk <anastasiia.aliinyk@gmail.com>
2021-06-13 21:08:22 +0300

    restored previous version

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
+
+gulp.task('sass', () => {
+    return gulp.src('./src/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./src/css'));
+});
+
+gulp.task('watch',  () => {
+    gulp.watch('./src/scss/**/*.scss', gulp.series('sass'));
+});
